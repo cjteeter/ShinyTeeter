fd7167edf43f4a7bd0ce61ed06eacdacceb3edb1
cjteeter <christopher.teeter@gmail.com>
2017-12-15 21:39:16 -0400

    update app code

--- 1_DecisionMaker/app.R
+++ 1_DecisionMaker/app.R
@@ -1,8 +1,10 @@
-##### Shiny App for Selecting an Option at Random #####
+##### Shiny App for Making a Decision by Random Choice #####
 
+# Load required packages -------------------------------
 require(shiny)
 require(shinythemes)
 
+# Create user interface -------------------------------
 ui <- fluidPage(theme = shinytheme('spacelab'),
         
         # App Title
@@ -22,9 +24,9 @@ ui <- fluidPage(theme = shinytheme('spacelab'),
         p(tags$b('Advanced'), ": Type the names of the options you are considering in a text-box (e.g., Futurama, Mad Men, Seinfeld). The decision-robot will return one of those options."),
         p("When you are ready for a decision to be made, press the", tags$em('Make Decision'), "button and the decision-robot will conjure its magic (read:", tags$code('randomly select'), "an option) for you so that you can 
           move forward with the rest of your day."),
-        p(tags$em('Note'), ": the", tags$em('Reset'), "button will reload the page if you want to start over and/or have it make another decision for you."),
+        p("The", tags$em('Reset'), "button will reload the page if you want to start over and/or have it make another decision for you."),
         hr(),
-
+        # User Input Section -------------------------------
         sidebarLayout(
                 sidebarPanel(
                         radioButtons(inputId = 'smp_adv',
@@ -42,8 +44,8 @@ ui <- fluidPage(theme = shinytheme('spacelab'),
                                                      value = 0,
                                                      step = 1),
                                          hr(),
-                                         actionButton(inputId = 's_generate', label = 'Make Decision'),
-                                         actionButton(inputId = 's_reset', label = 'Reset')),
+                                         actionButton(inputId = 's_generate', label = 'Make Decision', icon = icon("gavel")),
+                                         actionButton(inputId = 's_reset', label = 'Reset', icon = icon("refresh"))),
                         conditionalPanel(condition = "input.smp_adv == '2'",
                                          textInput(inputId = 'option_list',
                                                    label = 'Enter the name of an option and click the Add button'),
@@ -51,10 +53,10 @@ ui <- fluidPage(theme = shinytheme('spacelab'),
                                          hr(),
                                          verbatimTextOutput(outputId = 'user_list'),
                                          hr(),
-                                         actionButton(inputId = 'a_generate', label = 'Make Decision'),
-                                         actionButton(inputId = 'a_reset', label = 'Reset')),
+                                         actionButton(inputId = 'a_generate', label = 'Make Decision', icon = icon("gavel")),
+                                         actionButton(inputId = 'a_reset', label = 'Reset', icon = icon("refresh"))),
                         hr()),
-                
+                # Output of Plot, Data, and Summary -------------------------------
                 mainPanel(tags$style(type='text/css', "#s_winner {
                                         font-family: 'Arial';
                                         font-size: 350%;
@@ -77,16 +79,24 @@ ui <- fluidPage(theme = shinytheme('spacelab'),
                                            hr(),
                                            verbatimTextOutput(outputId = 'a_winner'))
                 )
-        )
+        ),
+        # Footer -------------------------------
+        tags$br(),
+        hr(),
+        p("App created by ", tags$a(href = "http://www.cteeter.ca", 'Chris Teeter', target = '_blank'), " in October 2017", HTML("&bull;"), "Follow Chris on Twitter:", tags$a(href = "https://twitter.com/c_mcgeets", tags$i(class = 'fa fa-twitter'), target = '_blank'),
+          HTML("&bull;"), "Find the code on Github:", tags$a(href = "https://github.com/cjteeter/ShinyTeeter/tree/master/1_DecisionMaker", tags$i(class = 'fa fa-github', style = 'color:#5000a5'), target = '_blank'), style = "font-size: 85%"),
+        p("Have a question? Send an email ", tags$a(href = "mailto:christopher.teeter@gmail.com", tags$i(class = 'fa fa-envelope', style = 'color:#990000'), target = '_blank'), style = "font-size: 85%"),
+        p(tags$em("Last updated: December 2017"), style = 'font-size:75%')
 )
 
+# Server logic -----------------------------------
 server <- function(input, output, session) {
         
-        # Output for Simple Option
+        # Output for Simple Option -----------------------------------
         observeEvent(input$s_reset, { session$reload() } )
         output$s_winner <- eventReactive(input$s_generate, { sample(1:input$num_options, 1) } )
         
-        # Output for Advanced Option
+        # Output for Advanced Option -----------------------------------
         observeEvent(input$a_reset, { session$reload() } )
         user_options <- reactiveValues()
         observe({
@@ -106,7 +116,5 @@ server <- function(input, output, session) {
         outputOptions(output, 'a_winner', suspendWhenHidden=FALSE)
 }
 
+# Run app -------------------------------
 shinyApp(ui = ui, server = server)
-
-
-                        
